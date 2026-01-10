2b4b36b5cc1e16aab0d7b05ccc1b6683bb76a305
Alexander Bakker <ab@alexbakker.me>
2026-01-10 17:16:58 +0100

    Update vendorHash to fix the Nix build

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
           name = "alertmanager-ntfy";
           src = self;
 
-          vendorHash = "sha256-CpVGLM6ZtfYODhP6gzWGcnpEuDvKRiMWzoPNm2qtML4=";
+          vendorHash = "sha256-1M0H/d6aBA8sKhVp8U9VL7mb09+q1sMdoCI2eg9/F9U=";
         };
       };
       devShells.default = with pkgs; mkShell {
